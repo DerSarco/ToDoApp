35303f343efd29d93ec40c1570f9bd4f8ba17e85
Carlos Muñoz Bustamante <carlos.a.munoz2@outlook.com>
2021-12-03 13:16:27 -0300

    added ToDo viewmodel and lifecycle coroutines dependency

---
+++ app/src/main/java/com/sarco/todoapp/data/viewModel/ToDoViewModel.kt
@@ -0,0 +1,31 @@
+package com.sarco.todoapp.data.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.sarco.todoapp.data.ToDoDatabase
+import com.sarco.todoapp.data.models.ToDoData
+import com.sarco.todoapp.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application): AndroidViewModel(application) {
+
+    private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+    private val repository: ToDoRepository = ToDoRepository(toDoDao)
+
+    private val getAllData: LiveData<List<ToDoData>>
+
+    init {
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+
+
+    }
+}
