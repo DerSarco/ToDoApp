96df441cf71d9396b4f919da9a057a1a41a2e728
Carlos Muñoz Bustamante <carlos.a.munoz2@outlook.com>
2021-12-03 12:58:24 -0300

    added todo database instance, singleton object

---
+++ app/src/main/java/com/sarco/todoapp/data/ToDoDatabase.kt
@@ -0,0 +1,37 @@
+package com.sarco.todoapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ToDoData::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase: RoomDatabase() {
+
+    abstract fun toDoDao(): ToDoDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: ToDoDatabase? = null
+
+        fun getDatabase(context: Context): ToDoDatabase {
+            val tempInstace = INSTANCE
+            if(tempInstace != null){
+                return tempInstace
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ToDoDatabase::class.java,
+                    "todo_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+    }
+
+}
