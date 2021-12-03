7a03a4a4de33d34dced139381394506ffc02390c
Carlos Muñoz Bustamante <carlos.a.munoz2@outlook.com>
2021-12-03 12:50:02 -0300

    Added Dao with two functions, getAllData and insertData

---
+++ app/src/main/java/com/sarco/todoapp/data/ToDoDao.kt
@@ -0,0 +1,18 @@
+package com.sarco.todoapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ToDoDao {
+
+    @Query("SELECT * FROM todo_table Order by id ASC")
+    fun getAllData(): LiveData<List<ToDoData>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertData(toDoData: ToDoData)
+
+}
